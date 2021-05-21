fd0716596508e414207375c8f9536765c3803df9
yoelgott <Yoelgott1998>
2021-05-21 07:27:50 -0700

    config updated

--- config.py
+++ config.py
@@ -1,11 +1,10 @@
 from enum import Enum
 
-DB_FILE = "db/rafael.db"
+CHUNK_SIZE = 2000
 
+DB_FILE = "db/rafael.db"
 ADS_TABLE_NAME = "ads"
 RESULTS_TABLE_NAME = "results"
-
-CHUNK_SIZE = 2000
 
 QUERY_ADS = f"""select * from {ADS_TABLE_NAME}"""
 QUERY_RESULTS = f"""select * from {RESULTS_TABLE_NAME}"""
